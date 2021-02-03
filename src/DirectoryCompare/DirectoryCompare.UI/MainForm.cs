48e385eed80da0f5c1de440dc51d817913a55c84
Mitchell Scobell <mitchscobell@gmail.com>
2021-02-03 13:30:30 -0500

    removed unneccessary usings

--- src/DirectoryCompare/DirectoryCompare.UI/MainForm.cs
+++ src/DirectoryCompare/DirectoryCompare.UI/MainForm.cs
@@ -1,12 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Security.Cryptography;
-using System.Text;
 using System.Windows.Forms;
 
 namespace DirectoryCompare.UI
